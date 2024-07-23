ce8f81da8e87af0d88602095221432495bb4d994
fikibamba <1116.frkn@gmail.com>
2024-07-23 03:24:10 +0300

    added html files

--- savDergi/base/urls.py
+++ savDergi/base/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('amac_kapsam', views.amac_kapsam, name='amac_kapsam')
+    path('amaç-ve-kapsam', views.amac_kapsam, name='amac_kapsam'),
+    path('misyonumuz', views.misyonumuz, name='misyonumuz'),
+    path('etik-ilkeler-ve-yayım-politikası', views.etik_ilkeler, name='etik_ilkeler'),
+    path('yazım-kuralları', views.yazim_kurallari, name='yazim_kurallari'),
 ]
--- savDergi/base/views.py
+++ savDergi/base/views.py
@@ -7,3 +7,16 @@ def home(request):
 
 def amac_kapsam(request):
     return render(request, 'base/amac_kapsam.html')
+
+
+def misyonumuz(request):
+    return render(request, 'base/misyonumuz.html')
+
+
+def etik_ilkeler(request):
+    return render(request, 'base/etik_ilkeler_ve_yayım_politikası.html')
+
+
+def yazim_kurallari(request):
+    return render(request, 'base/yazım_kuralları.html')
+
