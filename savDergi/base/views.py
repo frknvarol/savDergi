5648ace71b9187d8060a64f9f3b4066067eff8b7
fikibamba <1116.frkn@gmail.com>
2024-07-27 02:31:48 +0300

    organized css files and started etik ilkeler

--- savDergi/base/views.py
+++ savDergi/base/views.py
@@ -6,17 +6,21 @@ def home(request):
 
 
 def amac_kapsam(request):
-    return render(request, 'base/amac_kapsam.html')
+    context = {'page_css': 'savDergi/css/amac_kapsam.css'}
+    return render(request, 'base/amac_kapsam.html', context)
 
 
 def misyonumuz(request):
-    return render(request, 'base/misyonumuz.html')
+    context = {'page_css': 'savDergi/css/misyonumuz.css'}
+    return render(request, 'base/misyonumuz.html', context)
 
 
 def etik_ilkeler(request):
-    return render(request, 'base/etik_ilkeler_ve_yayım_politikası.html')
+    context = {'page_css': 'savDergi/css/etik_ilkeler_ve_yayım_politikası.css'}
+    return render(request, 'base/etik_ilkeler_ve_yayım_politikası.html', context)
 
 
 def yazim_kurallari(request):
+    context = {'page_css': 'savDergi/css/yazım_kuralları.css'}
     return render(request, 'base/yazım_kuralları.html')
 
