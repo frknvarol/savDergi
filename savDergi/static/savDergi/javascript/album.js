71d8a2488088b66cb5769500be219f9e0b7a421d
fikibamba <1116.frkn@gmail.com>
2024-08-11 13:07:21 +0300

    gallery development continued

--- savDergi/static/savDergi/javascript/album.js
+++
@@ -1,7 +0,0 @@
-const figure = document.querySelectorAll('.album_figure')
-
-figure.forEach(figure => {
-    const image = figure.querySelector('.album_image')
-    figure.style.height = image.offsetHeight + 'px'
-
-})
